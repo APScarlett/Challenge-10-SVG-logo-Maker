6cfef566fb2bb2146b1d4fec0f97b12a9433c7ab
Alexander Scarlett <ascarlett92@yahoo.com>
2024-05-25 03:07:53 -0400

    Added first message as well as choices

---
+++ index.js
@@ -0,0 +1,18 @@
+//include any packages needed for the application
+const inquier=require("inquirer")
+const fs=require("fs")
+const Triangle =require()
+//bellow is an array of questions for user input
+const questions=[
+    {
+        type:"list",
+        message:"Please choose from the following shapes",
+        name:"shape",
+        choices:["circle","triangle","square"]
+    },
+    {
+        type:
+        message:
+        name:
+    }
+]
