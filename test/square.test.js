6f883f4a7dbdb12af5527d5fa74a8efc3a4c6c3c
Alexander Scarlett <ascarlett92@yahoo.com>
2024-05-26 16:09:41 -0400

    updated square.js and square.test.js

--- test/square.test.js
+++ test/square.test.js
@@ -12,7 +12,8 @@ describe("Circle", ()=>{
     })
     describe("(method (is function in an object)",()=>{
         it("test render()",()=>{
-            expect(square.render()).toEqual(`<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"><square cx="150" cy="100" r="80" fill="yellow"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="purple">TYY</text></svg>`)
+            expect(square.render()).toEqual(`<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"><rect x="10" y="10" width="300" height="200"  fill="yellow" stroke-width="5"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="purple">TYY</text></svg>`)
         })
     })
 })
+
